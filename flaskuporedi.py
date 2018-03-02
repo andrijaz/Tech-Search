95b8b7f02cf3a8d0d9ef0dd8d181bf096b0a637f
andrijaz <andrijazivkovic@hotmail.com>
2018-03-02 23:23:46 +0100

    skorasnje pretrage

--- flaskuporedi.py
+++ flaskuporedi.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+# https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_desc
+# filter po ceni i ako i ma previse / premalo proizvoda nesto uraditi
 from flask import Flask, render_template, request
 import uporedi
 app = Flask(__name__)
 proceseed_text = ""
-
+pretrage = []
 
 @app.route("/")
 def index():
@@ -12,16 +14,22 @@ def index():
 @app.route('/', methods=['POST'])
 def index_post():
     text = request.form['text']
-    processed_text = text.lower()
+    proceseed_text = text.lower()
+
     uporedi.tehnomanija = []
     uporedi.gigatron = []
-    uporedi.get_tehnomanija(processed_text)
-    uporedi.get_gigatron(processed_text)
+    uporedi.get_tehnomanija(proceseed_text)
+    uporedi.get_gigatron(proceseed_text)
+
+    if proceseed_text not in pretrage:
+        pretrage.append(proceseed_text)
+    #else:
+     #   pretrage.pop(
 
     gigatron = uporedi.gigatron
     tehnomanija = uporedi.tehnomanija
 
-    return render_template("index.html", tehnomanija=tehnomanija, gigatron=gigatron)
+    return render_template("index.html", tehnomanija=tehnomanija, gigatron=gigatron, pretrage=pretrage, rec = proceseed_text)
 if __name__ == '__main__':
     #app.debug = True
     app.run()

da30fc8d23a29fcfe81dffc4410b9750a8fe9890
andrijaz <andrijazivkovic@hotmail.com>
2018-03-08 22:31:23 +0100

    dodat multi-pretaga, link,slike

--- flaskuporedi.py
+++ flaskuporedi.py
@@ -7,33 +7,71 @@ app = Flask(__name__)
 proceseed_text = ""
 pretrage = []
 
+nizu_izabrane = []
 @app.route("/")
 def index():
     return render_template("welcome.html")
 
 @app.route('/', methods=['POST'])
 def index_post():
+    izabrane = []
     text = request.form['text']
     proceseed_text = text.lower().strip()
 
-    uporedi.tehnomanija = []
-    uporedi.gigatron = []
-    uporedi.get_tehnomanija(proceseed_text)
-    uporedi.get_gigatron(proceseed_text)
 
-    if proceseed_text not in pretrage:
-        pretrage.append(proceseed_text)
-    #if (T in izabrane)
-    #   uporedi.tehnomanija = [], uporedi.get_tehnomanija(text), ime1 = t, firma1=t
-    #elif (G in izabrane)
-    #   uporedi.gigatron = [], uporedi.get_gigatron(text), ime1 = t, firma1=g
-    #
-    ime1 = "Tehnomanija"
-    ime2 = "Gigatron"
-    ime3 = "WinWin"
-    firma1 = uporedi.tehnomanija
-    firma2 = uporedi.gigatron
-    return render_template("index.html",ime1=ime1, ime2=ime2, firma1=firma1, firma2=firma2, pretrage=pretrage, rec = proceseed_text)
+    if len(proceseed_text)==0:
+        return render_template("welcome.html", poruka='Unesi pojam za pretragu')
+    if request.form.get('tehnomanija'):
+       izabrane.append('t')
+    if request.form.get('gigatron'):
+       izabrane.append('g')
+    if request.form.get('winwin'):
+       izabrane.append('w')
+    print(izabrane)
+    if len(izabrane) != 2 :
+        return render_template("welcome.html", poruka='Izaberi 2 firme')
+    else:
+        #poruka = 'Prikazujem sve ' + proceseed_text +' koje sam nasao'
+
+        if proceseed_text not in pretrage:
+            pretrage.append(proceseed_text)
+        if 'g' not in izabrane: #then tehno + winwin
+            uporedi.tehnomanija = []
+            uporedi.get_tehnomanija(proceseed_text)
+            ime1 = 'Tehnomanija'
+            firma1 = uporedi.tehnomanija
+
+            uporedi.winwin = []
+            uporedi.get_winwin(proceseed_text)
+            ime1 = 'Win Win'
+            firma1 = uporedi.winwin
+        if 't' not in izabrane: #then winwin + giga
+            uporedi.winwin = []
+            uporedi.get_winwin(proceseed_text)
+            ime1 = 'Win Win'
+            firma1 = uporedi.winwin
+
+            uporedi.gigatron = []
+            uporedi.get_gigatron(proceseed_text)
+            ime2 = "Gigatron"
+            firma2 = uporedi.gigatron
+
+        if 'w' not in izabrane: #then giga+tehno
+            uporedi.tehnomanija = []
+            uporedi.get_tehnomanija(proceseed_text)
+            ime1 = 'Tehnomanija'
+            firma1 = uporedi.tehnomanija
+
+            uporedi.gigatron = []
+            uporedi.get_gigatron(proceseed_text)
+            ime2 = "Gigatron"
+            firma2 = uporedi.gigatron
+
+        return render_template("index.html",ime1=ime1, ime2=ime2,
+                           firma1=firma1, firma2=firma2,
+                           pretrage=pretrage,
+                           rec = proceseed_text)
+
 if __name__ == '__main__':
     #app.debug = True
     app.run()
--- uporedi.py
+++ uporedi.py
@@ -6,46 +6,51 @@ from bs4 import BeautifulSoup
 def get_tehnomanija(fraza):
 
     link = "https://www.tehnomanija.rs/index.php?mod=catalog&op=thm_search&search_type=&submited=1&keywords="
-    fraze = fraza
-    fraze = fraze.replace(" ","+")
-    r = requests.get(link+fraze+"&items_per_page=60")
-    link = r.text
 
-    soup = BeautifulSoup(link, 'html.parser')
+    fraza = fraza.replace(" ","+")
+    r = requests.get(link+fraza+"&items_per_page=60")
+    link_text = r.text
+
+    soup = BeautifulSoup(link_text, 'html.parser')
 
     for p in soup.find_all("div", class_="product-wrap-grid"):
         ime = p.find("div", class_ = "product-name-grid").text.strip()
-        cena = p.find("div", class_="price").text.strip()
-        tehnomanija.append({"ime":ime, "cena":cena})
+        cena = int(p.find("div", class_="price").text.strip().strip("RSD").replace(".", ""))
+        link = p.find("a", class_="product-link").get('href')
+        img_link = p.find("img")['src']
+        tehnomanija.append({"ime":ime, "cena":cena, "link":link, "slika":img_link})
 
 def get_gigatron(fraza):
 
-    linkg = "https://www.gigatron.rs/pretraga?pojam="
-    frazeg = fraza
-    frazeg = frazeg.replace(" ", "+")
+    link = "https://www.gigatron.rs/pretraga?pojam="
+    fraza = fraza.replace(" ", "+")
 
-    r = requests.get(linkg+frazeg + "&prikaz=grid-view&limit=128")
-    linkg = r.text
+    r = requests.get(link+fraza + "&prikaz=grid-view&limit=128")
+    link_text = r.text
 
-    soupg = BeautifulSoup(linkg, 'html.parser')
+    soup = BeautifulSoup(link_text, 'html.parser')
 
-    for p in soupg.find_all("li", class_="item"):
+    for p in soup.find_all("li", class_="item"):
         ime = p.find("a",class_="product-name" ).text.strip()
 
         if p.find("h5", class_="old-price")== None:
             cena = p.find("h4", class_="final-price").text.strip() #cena bez maloprodajnog popust ili snizenja
         else:
             cena = p.find("h5", class_="old-price").text.strip()  # cena za maloprodajno
-        gigatron.append({"ime": ime, "cena": cena})
+        cena = cena.strip("RSD").strip("MP cena:").replace(".", "")
+
+        link = p.find("a", itemprop="url").get('href')
+        img_link = "https://www.gigatron.rs" + p.find("img")['src']
+
+        gigatron.append({"ime": ime, "cena": cena,  "link":link, "slika":img_link})
 
 def get_winwin(fraza):
     link  = "http://www.winwin.rs/catalogsearch/result/index/?limit=28&mode=grid&q="
-    fraze = fraza
-    fraze = fraze.replace(" ","+")
-    r = requests.get(link+fraze)
-    link = r.text
 
-    soup = BeautifulSoup(link, 'html.parser')
+    fraza = fraza.replace(" ","+")
+    r = requests.get(link+fraza)
+    link_text = r.text
+    soup = BeautifulSoup(link_text, 'html.parser')
     for p in soup.find_all("li", class_="item"):
         ime = p.find("a", itemprop="name").text.strip()
         cena = p.find("span", class_="price").text.strip()
@@ -55,3 +60,5 @@ def get_winwin(fraza):
 tehnomanija=[]
 gigatron = []
 winwin = []
+
+#print(gigatron)
